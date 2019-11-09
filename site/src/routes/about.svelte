7a8a433b79557e8e49fce43313d02cce57f99aaf
Sage Raflik <neonfuz@gmail.com>
2019-11-09 00:43:17 -0600

    Initial commit

---
+++ site/src/routes/about.svelte
@@ -0,0 +1,54 @@
+<svelte:head>
+	<title>About</title>
+</svelte:head>
+
+<style>
+ .round {
+     border-radius: 50%;
+     margin: 2em;
+ }
+ .container {
+     display: flex;
+     flex-direction: row;
+     flex-wrap: wrap;
+ }
+ figcaption {
+     text-align: center;
+     font-size: 2rem;
+ }
+</style>
+
+<h1>About us</h1>
+
+<div class="container">
+    <figure>
+        <img class="round" alt="Head 1" src="https://sophosnews.files.wordpress.com/2013/08/facebook-silhouette_thumb.jpg" />
+        <figcaption>
+            Person 1
+        </figcaption>
+    </figure>
+    <figure>
+        <img class="round" alt="Head 1" src="https://sophosnews.files.wordpress.com/2013/08/facebook-silhouette_thumb.jpg" />
+        <figcaption>
+            Person 2
+        </figcaption>
+    </figure>
+    <figure>
+        <img class="round" alt="Head 1" src="https://sophosnews.files.wordpress.com/2013/08/facebook-silhouette_thumb.jpg" />
+        <figcaption>
+            Person 3
+        </figcaption>
+    </figure>
+    <figure>
+        <img class="round" alt="Head 1" src="https://sophosnews.files.wordpress.com/2013/08/facebook-silhouette_thumb.jpg" />
+        <figcaption>
+            Person 4
+        </figcaption>
+    </figure>
+    <figure>
+        <img class="round" alt="Head 1" src="https://sophosnews.files.wordpress.com/2013/08/facebook-silhouette_thumb.jpg" />
+        <figcaption>
+            Person 5
+        </figcaption>
+    </figure>
+</div>
