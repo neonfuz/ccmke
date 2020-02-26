22c09b3bbbf3c4f64564014a32d0010a6c83a7f2
Sage Raflik <neonfuz@gmail.com>
2020-02-25 21:40:32 -0600

    LINT: graphql tabbing

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -166,39 +166,38 @@ export default IndexPage
 
 export const pageQuery = graphql`
   query IndexPageTemplate {
-        markdownRemark(frontmatter: {templateKey: {eq: "index-page" } }) {
-        frontmatter {
+    markdownRemark(frontmatter: {templateKey: {eq: "index-page" } }) {
+      frontmatter {
         title
         image {
-        childImageSharp {
-        fluid(maxWidth: 2048, quality: 100) {
-        ...GatsbyImageSharpFluid
+          childImageSharp {
+            fluid(maxWidth: 2048, quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
+        }
       }
-      }
-    }
-    heading
-    subheading
-        mainpitch {
+      heading
+      subheading
+      mainpitch {
         title
-          description
-}
-description
-        intro {
+        description
+      }
+      description
+      intro {
         blurbs {
-        image {
-        childImageSharp {
-        fluid(maxWidth: 240, quality: 64) {
-        ...GatsbyImageSharpFluid
-      }
+          image {
+            childImageSharp {
+              fluid(maxWidth: 240, quality: 64) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+          name
+          role
+        }
+        heading
+        description
       }
     }
-    name
-    role
   }
-  heading
-  description
-}
-}
-}
-}
-`
+}`
