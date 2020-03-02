0f9eb283aef515ab63fe25945aa197c92cc47d49
Sage Raflik <neonfuz@gmail.com>
2020-03-02 17:59:14 -0600

    Fixed up hero a bit

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -17,10 +17,10 @@ const Hero = ({ image, logo }) => (
         `linear-gradient( rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6) ),url(${
         !!image.childImageSharp ? image.childImageSharp.fluid.src : image
         })`,
-      backgroundPosition: `center 75px`,
+      backgroundPosition: `top`,
       backgroundAttachment: `fixed`,
       backgroundSize: '100vw 52vw',
-      maxHeight: '58vw',
+      maxHeight: '52vw',
       height: '80vh',
     }}>
     <div className="hero-body" style={{
@@ -36,7 +36,6 @@ const Hero = ({ image, logo }) => (
         <img src={logo} alt="Creative Counseling of Milwaukee" style={{
           maxWidth: '70vw',
           maxHeight: '50vh',
-          margin: '8rem',
         }} />
       </div>
     </div>
