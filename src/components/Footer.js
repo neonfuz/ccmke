45b37bb6461d8d8a1f8fc07fbf7a2c0ae22d0990
Sage Raflik <neonfuz@gmail.com>
2020-03-25 13:33:08 -0600

    Updated footer, removed social, fixed contact padding

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,6 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import Logo from '../img/logo.svg'
-import facebook from '../img/social/facebook.svg'
-import instagram from '../img/social/instagram.svg'
-import twitter from '../img/social/twitter.svg'
-import vimeo from '../img/social/vimeo.svg'
 
 import styles from './Footer.css'
 
@@ -66,68 +62,38 @@ const Footer = class extends React.Component {
                       </li>
                     </ul>
                   </section>
-                  <div className="social">
-                    <a title="facebook" href="https://facebook.com">
-                      <img
-                        src={facebook}
-                        alt="Facebook"
-                        style={{ width: '1em', height: '1em' }}
-                      />
-                    </a>
-                    <a title="twitter" href="https://twitter.com">
-                      <img
-                        className="fas fa-lg"
-                        src={twitter}
-                        alt="Twitter"
-                        style={{ width: '1em', height: '1em' }}
-                      />
-                    </a>
-                    <a title="instagram" href="https://instagram.com">
-                      <img
-                        src={instagram}
-                        alt="Instagram"
-                        style={{ width: '1em', height: '1em' }}
-                      />
-                    </a>
-                    <a title="vimeo" href="https://vimeo.com">
-                      <img
-                        src={vimeo}
-                        alt="Vimeo"
-                        style={{ width: '1em', height: '1em' }}
-                      />
-                    </a>
-                  </div>
-
                 </div>
               </div>
             </div>
             <div className="column">
-              <h2>Contact Us:</h2>
-              <div className="field">
-                <div className="label">Name</div>
-                <div className="control">
-                  <input className="input" name="name" type="name" value=""/>
+              <section className="section">
+                <h2>Contact Us:</h2>
+                <div className="field">
+                  <div className="label">Name</div>
+                  <div className="control">
+                    <input className="input" name="name" type="name" value=""/>
+                  </div>
                 </div>
-              </div>
-              <div className="field">
-                <label className="label">Email</label>
-                <div className="control has-icons-left">
-                  <input className="input" name="email" type="email" value="" />
-                  <span className="icon is-small is-left">
-                    <i className="fas fa-envelope"></i>
-                  </span>
+                <div className="field">
+                  <label className="label">Email</label>
+                  <div className="control has-icons-left">
+                    <input className="input" name="email" type="email" value="" />
+                    <span className="icon is-small is-left">
+                      <i className="fas fa-envelope"></i>
+                    </span>
+                  </div>
                 </div>
-              </div>
-              <div className="field">
-                <label className="label">Message</label>
-                <div className="field-body">
-                  <div className="field">
-                    <div className="control">
-                      <textarea className="textarea" name="message"></textarea>
+                <div className="field">
+                  <label className="label">Message</label>
+                  <div className="field-body">
+                    <div className="field">
+                      <div className="control">
+                        <textarea className="textarea" name="message"></textarea>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
+              </section>
             </div>
           </div>
         </div>
