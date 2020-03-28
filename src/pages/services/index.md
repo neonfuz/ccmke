9b7296e5e7914e678df9dfead6bff7700cfdc22d
Sage Raflik <neonfuz@gmail.com>
2020-03-28 17:28:31 -0600

    Services page update

--- src/pages/services/index.md
+++ src/pages/services/index.md
@@ -1,6 +1,29 @@
 ---
-templateKey: 'about-page'
+templateKey: 'services-page'
 path: /services
 title: Services
+services:
+  image: /img/services.png
+  list:
+    - name: Child-Centered Play Therapy
+      link: /services#play-therapy
+    - name: Sand Tray Therapy
+      link: /services#sand-tray-therapy
+    - name: Art Therapy
+      link: /services#art-therapy
+    - name: Theraplay
+      link: /services#theraplay
+    - name: Animal-Centered Therapy
+      link: /services#animal-centered-therapy
+    - name: Music & Movement
+      link: /services#music-and-movement
+    - name: Narrative/Life Journaling
+      link: /services#narrative-life-journaling
+    - name: Meditation & Mindfulness
+      link: /services#meditation-and-mindfulness
+    - name: Trauma-Informed Yoga
+      link: /services#trauma-informed-yoga
+    - name: NIA Dance
+      link: /services#nia-dance
 ---
 ### Work in progress...
---
+++ src/templates/services-page.js
@@ -0,0 +1,74 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { graphql } from 'gatsby'
+
+import Layout from '../components/Layout'
+import Content, { HTMLContent } from '../components/Content'
+import Services from '../components/Services'
+
+export const ServicesPageTemplate = ({
+  services,
+  content,
+}) => {
+  return (
+    <div className="section">
+      <Services {...{services}} />
+      <hr />
+      <div className="container">
+        <HTMLContent className="content" content={content} />
+      </div>
+    </div>
+  )
+}
+
+ServicesPageTemplate.propTypes = {
+  services: PropTypes.object,
+  content: PropTypes.string
+}
+
+const ServicesPage = ({ data }) => {
+  const { frontmatter, html } = data.markdownRemark
+
+  return (
+    <Layout>
+      <ServicesPageTemplate
+        services={frontmatter.services}
+        content={html}
+      />
+    </Layout>
+  )
+}
+
+ServicesPage.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.shape({
+      frontmatter: PropTypes.object,
+      html: PropTypes.string,
+    }),
+  }),
+}
+
+export default ServicesPage
+
+export const pageQuery = graphql`
+  query ServicesPageTemplate {
+    markdownRemark(frontmatter: {templateKey: {eq: "services-page" } }) {
+      frontmatter {
+        services {
+          list {
+            name
+            link
+          }
+          image {
+            childImageSharp {
+              fluid(maxWidth: 830, quality: 95) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      }
+      html
+    }
+  }
+`
