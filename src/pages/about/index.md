aa5ac0e6219e869f3bdeecbc514a2c12981ec2a7
Sage Raflik <neonfuz@gmail.com>
2020-04-07 14:00:58 -0600

    About page work

--- src/pages/about/index.md
+++ src/pages/about/index.md
@@ -1,7 +1,34 @@
 ---
 templateKey: about-page
 title: About Us
+intro:
+  image: /img/xd/Diversity.png
+  imageText: diversity
+  body: >
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+    non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+therapists:
+    - id: sarah-la-follette 
+      image: /img/xd/mom cc.png
+      name: Sarah La Follette, LCSW
+      phone: (414)-322-2807
+      email: LaFolletteSarah@gmail.com
+      specialties:
+        - Trauma and PTSD
+        - Depression
+        - Bipolar Disorder
+      body: >
+        placeholder text
+    - id: therapist-2 
+      image: /img/profiles/placeholder.png 
+      name: therapist 2
+      phone: (000)-000-0000
+      email: foo@bar.com
+      body: >
+        more text
+     
 ---
-### Work in progress...
-
-Therapist bios go here
--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -3,8 +3,89 @@ import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import Content, { HTMLContent } from '../components/Content'
+import Image from '../components/PreviewCompatibleImage'
 
-export const AboutPageTemplate = ({ title, content, contentComponent }) => {
+const Intro = ({image, imageText, body}) => (
+  <div className="columns">
+    <div className="column is-4">
+      <Image imageInfo={{image, alt: imageText}} />
+      {/* TODO: Replace this with loading image */}
+      {/* TODO: style text more */}
+    </div>
+    <div className="column is-8" style={{
+      fontSize: '1.8rem',
+      lineHeight: '1.5em',
+    }}>
+      {body}
+    </div>
+  </div>
+)
+
+Intro.propTypes = {
+  image: PropTypes.string,
+  imageText: PropTypes.string,
+  body: PropTypes.string,
+}
+
+const TherapistSection = ({id, image, name, phone, email, specialties, body}) => (
+  <>
+    <div className="section" id={id}>
+      <div style={{
+        float: 'left',
+        width: '33%',
+        marginRight: '2em',
+        marginBottom: '2em',
+        fontSize: '1.5rem'
+      }}>
+        <Image imageInfo={{image, alt: `${name} picture`}} />
+      </div>
+      <div style={{float: 'right'}}>
+        {!specialties ? null : (
+          <>
+            <div style={{fontWeight: 'bold'}}>Specialties:</div>
+            <ul style={{fontSize: '0.85em', marginLeft: '1em'}}>
+              {specialties.map(item => (<li key={item}>{item}</li>))}
+            </ul>
+          </>
+        )}
+      </div>
+      <div>
+        <div style={{
+          fontWeight: 'bold',
+        }}>{name}</div>
+        <div>{phone}</div>
+        <div>{email}</div>
+      </div>
+      <div className="is-clearfix" style={{fontSize: '0.9em', padding: '0.75rem'}} >
+        {body}
+      </div>
+    </div>
+    <hr />
+  </>
+)
+
+TherapistSection.propTypes = {
+  name: PropTypes.string,
+  image: PropTypes.string,
+  phone: PropTypes.string,
+  email: PropTypes.string,
+  specialties: PropTypes.arrayOf(
+    PropTypes.string
+  ),
+  body: PropTypes.string,
+}
+
+const Therapists = ({therapists}) => (
+  <>
+    {therapists.map(t => (
+      <TherapistSection key={t.id} {...t} />
+    ))}
+  </> // TODO: is okay to just return an array here?
+)
+
+
+
+export const AboutPageTemplate = ({ frontmatter, content, contentComponent }) => {
   const PageContent = contentComponent || Content
 
   return (
@@ -13,9 +94,12 @@ export const AboutPageTemplate = ({ title, content, contentComponent }) => {
         <div className="columns">
           <div className="column is-10 is-offset-1">
             <div className="section">
-              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
-                {title}
+              <h2 className="title is-size-2 has-text-weight-bold is-bold-light">
+                {frontmatter.title}
               </h2>
+              <Intro {...frontmatter.intro} />
+              <hr />
+              <Therapists therapists={frontmatter.therapists} />
               <PageContent className="content" content={content} />
             </div>
           </div>
@@ -26,7 +110,6 @@ export const AboutPageTemplate = ({ title, content, contentComponent }) => {
 }
 
 AboutPageTemplate.propTypes = {
-  title: PropTypes.string.isRequired,
   content: PropTypes.string,
   contentComponent: PropTypes.func,
 }
@@ -38,7 +121,7 @@ const AboutPage = ({ data }) => {
     <Layout>
       <AboutPageTemplate
         contentComponent={HTMLContent}
-        title={post.frontmatter.title}
+        frontmatter={post.frontmatter}
         content={post.html}
       />
     </Layout>
@@ -51,12 +134,39 @@ AboutPage.propTypes = {
 
 export default AboutPage
 
+// TODO: fix image widths
 export const aboutPageQuery = graphql`
   query AboutPage($id: String!) {
     markdownRemark(id: { eq: $id }) {
       html
       frontmatter {
         title
+        intro {
+          image {
+            childImageSharp {
+              fluid(maxWidth: 830, quality: 95) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+          imageText
+          body
+        }
+        therapists {
+          id
+          image {
+            childImageSharp {
+              fluid(maxWidth: 830, quality: 95) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+          name
+          phone
+          email
+          specialties
+          body
+        }
       }
     }
   }
