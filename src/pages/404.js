39747852bcfc7616e6a69e747156a5af23836b49
Sage Raflik <neonfuz@gmail.com>
2020-07-28 13:38:26 -0500

    Updated 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,8 +4,8 @@ import Layout from '../components/Layout'
 const NotFoundPage = () => (
   <Layout>
     <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <h1>404 Not Found</h1>
+      <p>Page not found...</p>
     </div>
   </Layout>
 )
