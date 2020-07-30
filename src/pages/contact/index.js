d87f5c327294c1cea1661658ce93ff9c70686917
Sage Raflik <neonfuz@gmail.com>
2020-07-29 19:38:11 -0500

    Made Contact header consistent with other headers

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -39,7 +39,7 @@ export default class Index extends React.Component {
         <section className="section">
           <div className="container">
             <div className="content">
-              <h1>Contact</h1>
+              <h1 className="title is-size-2">Contact</h1>
               <form
                 name="contact"
                 method="post"
